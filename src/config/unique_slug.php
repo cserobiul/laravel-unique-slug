334556a3cc10e2bb7d0842a58c731e402457c9bc
cserobiul <abishar14@gmail.com>
2022-12-02 22:21:44 +0600

    config file commenting

--- src/config/unique_slug.php
+++ src/config/unique_slug.php
@@ -1,8 +1,23 @@
 <?php
 
 return[
+    /*
+    |--------------------------------------------------------------------------
+    | Slug default separator.
+    |--------------------------------------------------------------------------
+    | If no separator is passed, then this default separator will be used as slug.
+    |
+    */
     'separator' => '-',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Slug max count limit
+    |--------------------------------------------------------------------------
+    | Default value 100, slug will generated like
+    | slug-1, slug-2, slug-3, slug-4... slug-100
+    |
+    */
     'max_count' => 100,
 ]
 
